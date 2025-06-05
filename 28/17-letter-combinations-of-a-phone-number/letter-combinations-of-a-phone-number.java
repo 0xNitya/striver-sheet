1e6e381935c3c4bf6cefcb5b4f9cdc71ab87dc69
Nityanand Yadav <nityanandswami2324@gmail.com>
2025-06-05 04:06:54 -0700

    Time: 0 ms (100.00%) | Memory: 42.1 MB (64.40%) - LeetSync

---
+++ 28/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()){return new ArrayList<>();}
+        List<String>ans=new ArrayList<>();
+        dfs(digits,0,new StringBuilder(),ans);
+        return ans;
+    }
+    private static final String[] digittoletter={"",  "",  "abc", "def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    private void dfs(String digits,int i,StringBuilder sb,List<String>ans)
+    {
+        if(i==digits.length())
+        {
+            ans.add(sb.toString());
+            return;
+        }
+        for(final char c:digittoletter[digits.charAt(i)-'0'].toCharArray())
+        {
+            sb.append(c);
+            dfs(digits,i+1,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
