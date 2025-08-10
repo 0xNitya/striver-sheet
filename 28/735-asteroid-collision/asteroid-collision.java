e9731b04ed5d8fa3aacd4f01bc86120c3b514448
Nityanand Yadav <nityanandswami2324@gmail.com>
2025-08-10 23:07:57 +0530

    Time: 6 ms (63.76%) | Memory: 45.4 MB (73.29%) - LeetSync

--- 28/735-asteroid-collision/asteroid-collision.java
+++ 28/735-asteroid-collision/asteroid-collision.java
@@ -1,27 +1,25 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<>();
-
-        for (int a : asteroids) {
+        //Input: asteroids = [5,10,-5]   Output: [5,10]
+        for (int a : asteroids) { //a=10
             boolean alive = true;
-
-            while (!st.isEmpty() && a < 0 && st.peek() > 0) {
-                if (st.peek() < -a) {
+            while (!st.isEmpty() && a < 0 && st.peek() > 0) {  // ++ a-> b->  a-> b<-
+                if (st.peek() < -a) {   // 
                     st.pop(); // top fatega
                 } else if (st.peek() == -a) {
-                    st.pop(); // both fatenge
+                    st.pop(); // both fatenge  // 5==-10
                     alive = false;
                     break;
                 } else {
-                    alive = false; // incoming fatega
+                    alive = false; // incoming fatega break
                     break;
                 }
             }
             if (alive) {
-                st.push(a);
+                st.push(a);  //(5,10)
             }
         }
-
         int[] res = new int[st.size()];
         for (int i = res.length - 1; i >= 0; i--) {
             res[i] = st.pop();
