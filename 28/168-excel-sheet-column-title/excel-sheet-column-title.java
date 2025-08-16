4322d5a93a0c3f5d9c375ad4b447599bdb5baf87
Nityanand Yadav <nityanandswami2324@gmail.com>
2025-08-16 19:08:20 +0530

    Time: 6 ms (13.34%) | Memory: 41.5 MB (9.04%) - LeetSync

---
+++ 28/168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        if(columnNumber==0){return "";}
+        else 
+        return convertToTitle((columnNumber - 1) / 26) + (char) ('A' + ((columnNumber - 1) % 26));
+    }
+}
