76e16d7137f22966330f1843a216ee88de20fb64
Nityanand Yadav <nityanandswami2324@gmail.com>
2025-08-28 23:54:51 +0530

    Time: 12 ms (62.36%) | Memory: 44.8 MB (43.22%) - LeetSync

---
+++ 28/482-license-key-formatting/license-key-formatting.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int ct = 0;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toUpperCase(c));
+                ct++;
+                if (ct == k) {
+                    sb.append('-');
+                    ct = 0;
+                }
+            }
+        }
+        // Agar last character '-' se end ho raha ho, remove it
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '-') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        
+        return sb.reverse().toString();
+    }
+}
